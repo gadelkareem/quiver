f091fc81bbeb0ae5df9964d8d9e4cedb69444ef7
Waleed Gadelkareem <gadelkareem@gmail.com>
2021-10-18 17:58:26 +0200

    update test

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.16
 require (
 	github.com/astaxie/beego v1.12.3
 	github.com/gadelkareem/go-helpers v0.0.0-20200725110331-77a434fc0423
+	github.com/gadelkareem/proxy.webshare.io v0.0.0-20211018143112-e0d299945f0f
 )
--- quiver_test.go
+++ quiver_test.go
@@ -1,23 +1,32 @@
-package quiver
+package quiver_test
 
-// func TestNewProxyFactory(t *testing.T) {
-// 	u := os.Getenv("PW_URL")
-// 	if u == "" {
-// 		panic("Please provide a proxy.webshare.io download list URL")
-// 	}
-// 	pwProxy, err := pw.NewClient(u, nil)
-// 	if err != nil {
-// 		t.Error(err)
-// 		return
-// 	}
-// 	p := NewProxyFactory(UseAllProxy, 10, false, true, true, "./testdata", "", "", pwProxy)
-// 	tl := p.TotalCount()
-// 	for i := 0; i < tl; i++ {
-// 		ip, u := p.RandomProxy()
-// 		println(ip, u.String())
-// 	}
-// 	fmt.Printf("Got %d proxies \n", tl)
-// 	if tl < 112 {
-// 		t.Error("Invalid number of proxies")
-// 	}
-// }
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	pw "github.com/gadelkareem/proxy.webshare.io"
+	"github.com/gadelkareem/quiver"
+)
+
+func TestNewProxyFactory(t *testing.T) {
+	u := os.Getenv("PW_URL")
+	if u == "" {
+		panic("Please provide a proxy.webshare.io download list URL")
+	}
+	pwProxy, err := pw.NewClient(u, nil)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	p := quiver.NewProxyFactory(quiver.UseAllProxy, 10, false, true, true, "./testdata", "", "", pwProxy)
+	tl := p.TotalCount()
+	for i := 0; i < tl; i++ {
+		ip, u := p.RandomProxy()
+		println(ip, u.String())
+	}
+	fmt.Printf("Got %d proxies \n", tl)
+	if tl < 260 {
+		t.Error("Invalid number of proxies")
+	}
+}
